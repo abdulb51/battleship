1344fbc90af8e4cc9b95feda4aa95df0f5e6d7c9
abdulb51 <abdulhadib51@kprschools.ca>
2026-09-16 14:30:01 -0400

    done

--- src/main.rs
+++ src/main.rs
@@ -17,72 +17,68 @@ opening, winning and losing messages>
 use rand::Rng;
 use std::io;
 
-
-
 fn main() {
+    let mut rng = rand::rng();
+    let random_num: i32 = rng.random_range(1..=3);
+    println!("Dice roll: {}", random_num);
+
     println!("Hello battleship!");
 
-let chances = 3;
-if chances == 0 {
-    println!("You have no chances left");
-    return;
-}
+    let chances = 3;
+    if chances == 0 {
+        println!("You have no chances left");
+        return;
+    }
 
-let mut rng = rand::thread_rng();
-let random_num = rng.gen_range(1..=3);
+    let mut ship1 = vec![];
+    let mut ship2 = vec![];
+    let mut ship3 = vec![];
 
-let mut spot1 = vec![ ];
-let mut spot2 = vec![ ];
-let mut spot3 = vec![ ];
+    let mut spots = vec![
+        "A1", "A2", "A3", "A4", "A5", "B1", "B2", "B3", "B4", "B5", "C1", "C2", "C3", "C4", "C5", "D1", "D2", "D3", "D4", "D5", "E1", "E2", "E3",
+        "E4", "E5",
+    ];
 
+    if random_num == 1 {
+        ship1.push("A1");
+        ship1.push("A2");
 
-let mut spots = vec!["A1", "A2", "A3", "A4", "A5", "B1", "B2", "B3", "B4", "B5", "C1", "C2", "C3", "C4", "C5", "D1", "D2", "D3", "D4", "D5", "E1", "E2", "E3", "E4", "E5"];
+        ship2.push("D1");
+        ship2.push("E1");
 
+        ship3.push("D3");
+        spots.push("D4");
+    } else if random_num == 2 {
+        ship1.push("A3");
+        ship1.push("B3");
 
+        ship2.push("C3");
+        ship2.push("D3");
 
+        ship3.push("E3");
+        ship3.push("E2");
+    } else if random_num == 3 {
+        ship1.push("A2");
+        ship1.push("B2");
 
-if random_num == 1 {
-spot1.push("A1");
-spot1.push("A2");
+        ship2.push("B4");
+        ship2.push("B5");
 
-spot2.push ("D1");
-spot2.push ("E1");
+        ship3.push("E4");
+        ship3.push("E5");
+    }
 
-spot3.push ("D3");
-spots.push ("D4");
-}
+    println!("Pick a spot. try to sink all the ship!");
+    // println!("Available spots: {:?}", spots);
 
-else if random_num == 2 {
-    spot1.push("A3");
-    spot1.push("B3");
-    
-    spot2.push ("C3");
-    spot2.push ("D3");
-    
-    spot3.push ("E3");
-    spot3.push ("E2");
-}
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read input");
 
-else if random_num == 3 {
-    spot1.push("A2");
-    spot1.push("B2");
-    
-    spot2.push ("B4");
-    spot2.push ("B5");
-    
-    spot3.push ("E4");
-    spot3.push ("E5");
-}
+    println!("You entered: {}", input);
 
-println!("Pick a spot. try to sink all the ship!")
-
-println!("Available spots: {:?}", spots);
-
-
-
-let mut input = String::new();
-io::stdin().read_line(&mut input).expect("Failed to read input");
-println!("You entered: {}", input);
-
-
+    if input == "a1" || input == "A1" && ship1.contains(&"A1") || ship2.contains(&"A1") || ship3.contains(&"A1") {
+        println!("You hit the ship!");
+    } else {
+        println!("You hit! the water..");
+    }
 }
